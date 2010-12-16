df94ba6143d6b3c33497fc099df6aaa93d8de635
alessandro.calzavara <alessandro.calzavara@4492702a-a82a-3524-4881-1b3163c32563>
2010-12-16 14:45:30 +0000

    Levo la cartella src, non va bene... Aggiungo 2 file php di esempio

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login</title>
+</head>
+<body>
+<h1>Accedi</h1>
+<form>
+  Nome Utente: <input type="text" name="username" size="15" /><br />
+  Password: <input type="password" name="password" size="15" /><br />
+  <div align="left">
+    <p><input type="submit" value="Login" /></p>
+  </div>
+</form>
+</body>
+</html>
