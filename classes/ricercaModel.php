2bdeff4d70364f44e3adcf07246a7333e7c07a11
LegendaryHiryu <LegendaryHiryu@4492702a-a82a-3524-4881-1b3163c32563>
2010-12-23 12:42:12 +0000

    modificati: ricerca.php ricercaModel.php ricercaView.php
    ricercaView.php : Ho scelto di distinguere fra ricerca semplice e avanzata mettendo nel pulsante submit come valore dell'attributo value "Ricerca Semplice" oppure "Ricerca Avanzata".
    typeOfSearch (in ricercaModel) sarà comunque utile per far visualizzare i form a seconda delle scelte dell'utente

--- classes/ricercaModel.php
+++ classes/ricercaModel.php
@@ -3,16 +3,21 @@
 require_once (dirname(__FILE__) . '/pageModel.php');
 require_once (dirname(__FILE__) . '/loginModel.php');
 
+require_once (dirname(__FILE__) . '/db_connector.php');
+
 class Ricerca extends Page {
 	
 	private $session;
+	
+	protected $search_result;
+	protected $search_error;
 	
 	public function __construct() {
 		parent::__construct();
 
 		// controllo che la sessione sia valida
 		$this->session = new LoginSession();
-		if (!$this->session->userIsLogged()) { // un utente  arrivato qua senza essere loggato, lo rispedisco a casa
+		if (!$this->session->userIsLogged()) { // un utente è arrivato qua senza essere loggato, lo rispedisco a casa
 			header("Location: login.php");
 		}
 
@@ -22,7 +27,64 @@ class Ricerca extends Page {
 		return $this->session->getUser();
 	}
 	
-	public function doBasicSearch($keys) {
+	
+	// ricerca semplice: crea la query da sottoporre a interrogateDB()
+	public function doSimpleSearch($keys) {
+		$queryString = 'SELECT ';
+		
+		/* TO DO:
+		 * estrae chiavi di ricerca da $keys e le concatena a $queryString
+		 */
+		
+		interrogateDB($queryString);
+	}
+	
+	// ricerca avanzata: crea la query da sottoporre a interrogateDB()
+	//	NB: la ricerca usa $_POST per comodità  (DA CONTROLLARE SE FUNZIONA!)
+	public function doAdvancedSearch() {
+		$queryString = 'SELECT ';
+		
+		/* TO DO:
+		 * estrae chiavi di ricerca dai vari $_POST[''] e le concatena a $queryString
+		 */
+		
+		interrogateDB($queryString);
+	}
+	
+	
+	//esegue l'interrogazione del DB attraverso la query fornita
+	public function interrogateDB($queryString)
+		// istanza della classe
+		$dbc = new DBConnector();
+		// chiamata alla funzione di connessione
+		$dbc->connect();
+		// interrogazione della tabella
+		$raw_data = $dbc->query($queryString);
+		
+		/* hack momentaneo finché il db non è pronto */
+			$this->search_result = ['Documento DQ','Allegato A1'];
+		/* hack momentaneo finché il db non è pronto */
+		
+	/*
+		if ($dbc->rows($raw_data)>0) {
+			// chiamata alla funzione per l'estrazione dei dati
+			$search_result =  $dbc->extract_object($raw_data);
+			unset($search_error);
+		} else {
+			$this->search_error = "La ricerca non ha prodotto risultati";
+		}
+	*/
+			
+		// disconnessione da MySQL
+		$dbc->disconnect();
+	}
+	
+	
+	// true se nessun parametro della ricerca avanzata è stato impostato
+	// false se almeno un parametro della ricerca avanzato è stato impostato
+	//	NB: usa $_POST per comodità  (DA CONTROLLARE SE FUNZIONA!)
+	public function noParameterIsSet() {
+		//controlli sui vari $_POST['']
 		return false;
 	}
 	
--- ricerca.php
+++ ricerca.php
@@ -2,21 +2,50 @@
 
 // Ricerca controller ...  l'utente chiede questa pagina
 
-// scarico il modello per questa pagina,  questo che il lavoro sporco
+// scarico il modello per questa pagina, è questo che il lavoro sporco
 require_once (dirname(__FILE__) . '/classes/ricercaModel.php');
 
 // inizializzo il modello per la ricerca
 $ricerca = new Ricerca();
 
-// controllo se l'utente mi sta inviando chiavi su cui eseguire la ricerca
-if (isset($_POST['submit']) && (trim($_POST['submit']) == "Cerca")) {
 
-	if (trim($_POST['parametriRicerca']) == "") {
-		$search_error = "Nessuna chiave di ricerca inserita";
-	} else {
-		$search_result = $ricerca->doBasicSearch($_POST['parametriRicerca']);
+/*controlla se l'utente ha inviato qualche parametro di ricerca e la esegue*/
+
+//l'utente ha inviato qualcosa?
+if (isset($_POST['submit']) {
+	
+	//l'utente ha richiesto una ricerca semplice?
+	if (trim($_POST['submit']) == "Ricerca Semplice") {
+		
+		//controlla se il parametro di ricerca non è stato impostato
+		if (trim($_POST['parametroRicerca']) == "") {
+			$search_error = "Nessuna chiave di ricerca inserita";
+		} else {
+			$ricerca->doSimpleSearch($_POST['parametroRicerca']);
+		}
 	}
+	//l'utente ha richiesto una ricerca avanzata?
+	elseif (trim($_POST['submit']) == "Ricerca Avanzata")) {
+		
+		/* chiama metodo che controlla se nessun parametro è stato impostato
+		 *  fa il corrispettivo di
+		 *  if (trim($_POST['parametriRicerca']) == "") {
+		 *  per la ricerca avanzata che ha più parametri
+		 */
+		if (noParameterIsSet()) {
+			$search_error = "Nessuna chiave di ricerca inserita";
+		} else {
+			$ricerca->doAdvancedSearch();
+		}
+	}
+	
 }
+
+//	se ci sono risultati in $search_result visualizza i risultati magari utilizzando
+//		###> header("Location: risultati.php"); <###
+//	dove il controller risultati.php si appoggia a view/risultatiView.php
+// altrimenti si può usare un metodo di ricercaModel
+
 
 // carico la vista da mostrare all'utente
 require ('view/ricercaView.php');
