f384270ad508a984769a7875b6bf187f18a12d39
LegendaryHiryu <LegendaryHiryu@4492702a-a82a-3524-4881-1b3163c32563>
2010-12-28 17:40:47 +0000

    ricercaView: nei gruppi di checkbox ho stabilito come valore dell'attributo "name" un array con nome comune anzichè nomi distinti
    ricercaModel:
    ho adoperato due metodi separati per settare ricerca avanzata e semplice
    rinominato il metodo typeOfSearch in getTypeOfSearch
    
    ricerca.php:
    implementata la funzione noParameterIsSet() in ricerca.php (da controllare CODICE ALTERNATIVO proposto)

--- trunk/ricerca.php
+++ trunk/ricerca.php
@@ -10,22 +10,28 @@ $ricerca = new Ricerca();
 
 // controllo che tipo di ricerca vuole fare l'utente
 if (isset($_GET['type'])) {
-	if (($_GET['type'] == "simple") || ($_GET['type'] == "advanced")) {
-		$ricerca->setSearchType($_GET['type']);
+	if (($_GET['type'] == "simple")	{
+		$ricerca->setSimpleSearch();
+	} elseif ($_GET['type'] == "advanced")) {
+		$ricerca->setAdvancedSearch();
 	}
 } elseif (isset($_POST['search_type'])) {
-	if (($_POST['search_type'] == "simple") || ($_POST['search_type'] == "advanced")) {
-		$ricerca->setSearchType($_POST['search_type']);
+	if (($_POST['search_type'] == "simple")	{
+		$ricerca->setSimpleSearch();
+	} elseif ($_POST['search_type'] == "advanced")) {
+		$ricerca->setAdvancedSearch();
 	}
 }
 
-/* controlla se l'utente ha inviato qualche parametro di ricerca e la esegue */
+//RICORDA: per impostazione default il tipo di ricerca Ë "simple" (vedi ricercaModel)
+
+/*controlla se l'utente ha inviato qualche parametro di ricerca e la esegue*/
 
 //l'utente ha inviato qualcosa?
 if (isset($_POST['submit'])) {
 	
 	//l'utente ha richiesto una ricerca semplice?
-	if ($ricerca->typeOfSearch() == "simple") {
+	if ($ricerca->getTypeOfSearch() == "simple") {
 		
 		//controlla se il parametro di ricerca non Ë stato impostato
 		if (trim($_POST['parametroRicerca']) == "") {
@@ -35,28 +41,124 @@ if (isset($_POST['submit'])) {
 		}
 	}
 	//l'utente ha richiesto una ricerca avanzata?
-	elseif ($ricerca->typeOfSearch() == "advanced") {
+	elseif ($ricerca->getTypeOfSearch() == "advanced") {
 		
-		/* chiama metodo che controlla se nessun parametro Ë stato impostato
+		/* chiama la funzione che controlla se nessun parametro Ë stato impostato
 		 *  fa il corrispettivo di
 		 *  if (trim($_POST['parametriRicerca']) == "") {
 		 *  per la ricerca avanzata che ha pi˘ parametri
-		 *  TODO: è da fare però!
+		 *  TODO: ? da fare però!
 		 */
 		if (noParameterIsSet()) {
 			$search_error = "Nessuna chiave di ricerca inserita";
 		} else {
-			$ricerca->doAdvancedSearch();
+			$ricerca->doAdvancedSearch( getAdvancedKeys() );
 		}
 	}
 	
 }
 
-// se c'è un solo risultato, faccio un redirect su visualizza.php?document_id=<id documento>
-// se ci sono più risultati, allora mostro l'elenco tramite la view risultatiView.php
+// se c'Ë un solo risultato, faccio un redirect su visualizza.php?document_id=<id documento>
+// se ci sono pi˘ risultati, allora mostro l'elenco tramite la view risultatiView.php
 // in tutti gli altri casi, mostro la view standard ricercaView.php
 
 // carico la vista da mostrare all'utente
 require ('view/ricercaView.php');
+
+
+//FUNZIONI
+
+// true: se nessun parametro della ricerca avanzata Ë stato impostato
+// false: se almeno un parametro della ricerca avanzato Ë stato impostato
+function noParameterIsSet() {
+		
+	//nessun campo dell'intestazione Ë stato impostato?
+	
+		//nessun campo dell'identificatore Ë stato impostato?
+		if (trim($_POST['identificatore']) == "") return false;
+		
+		foreach($_POST['classe'] as $value) {
+			if isset($value) return false;
+		}
+		
+		if (trim($_POST['versione']) == "") return false;
+		if (trim($_POST['anno']) == "") return false;
+		if (trim($_POST['numero']) == "") return false;
+		//a questo punto nessun campo dell'identificatore Ë stato impostato
+		
+	if (trim($_POST['data']) == "") return false;
+	if (trim($_POST['revisione']) == "") return false;
+	
+	foreach($_POST['stato'] as $value) {
+		if isset($value) return false;
+	}
+	
+	if (trim($_POST['lingua']) == "") return false;
+
+
+	//nessun campo del piÈ di pagina Ë stato impostato?
+	
+	if (trim($_POST['sede']) == "") return false;
+	
+	foreach($_POST['livello'] as $value) {
+		if isset($value) return false;
+	}
+	
+	if (trim($_POST['allegati']) == "") return false;
+	if (trim($_POST['pagine']) == "") return false;
+	if (trim($_POST['approvato']) == "") return false;
+	if (trim($_POST['autore']) == "") return false;
+	
+	
+	if (trim($_POST['abstract']) == "") return false;
+	
+	if (trim($_POST['doc']) == "") return false;
+	
+	
+	
+	/* CODICE ALTERNATIVO a tutti gli if
+	 * NB: i tre cicli foreach precedenti sono riportati come sopra!
+	 * TODO: verificare se potrebbe funzionare
+	
+	$fields = array('identificatore','versione','anno','numero','data','revisione','lingua','sede','allegati','pagine','approvato','autore','abstract','doc');
+	
+	foreach($fields as $field) {
+		if ( trim($_POST[$field]) == "" ) return false;
+	}
+	
+	// cicli foreach corrispondenti ai tre gruppi di checkbox
+	foreach($_POST['classe'] as $value) {
+			if isset($value) return false;
+	}
+	
+	foreach($_POST['livello'] as $value) {
+		if isset($value) return false;
+	}
+	
+	foreach($_POST['livello'] as $value) {
+		if isset($value) return false;
+	}
+	
+	
+	// FINE CODICE ALTERNATIVO
+	 */
+	
+	
+	//a questo punto nessuno dei precedenti parametri Ë stato impostato
+	return true;
+}
+	
+	
+	
+//costruisce la parte della query contenente le chiavi di una ricerca avanzata
+function getAdvancedKeys() {
+	
+	$partialQuery = "";
+	
+	/* TODO: estrae chiavi di ricerca dai vari $_POST[''] e costruisce $partialQuery
+	 */
+	
+	return $partialQuery;
+}
 
 ?>
